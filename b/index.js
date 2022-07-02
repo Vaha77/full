effd2407c1ec844927a4cfe1a08badbccd477f17
vaha77 <abdulvohid996@gmail.com>
2022-07-02 16:16:04 +0900

    vaha

--- b/index.js
+++ b/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 
 const cors = require("cors");
+
+const mongoose = require("mongoose");
+
 const app = express();
 app.use(cors());
+
+mongoose.connect("mongodb://localhost:27017");
+
+// const db = mongoose.connection;
+
+// db.on("connected", () => console.log("conected to mongodb"));
+// db.on("error", () => console.log("error"));
+// db.on("discounted", () => console.log("discounted"));
 
 const hauz = require("./hauz");
 
